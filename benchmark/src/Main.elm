81bd6e6c3c285a28fc47f49f15ab1da7230d8df3
Martin Stewart <MartinSStewart@gmail.com>
2020-08-05 21:36:22 +0200

    Add old serializer to tests and benchmark

--- benchmark/src/Main.elm
+++ benchmark/src/Main.elm
@@ -1,25 +1,63 @@
 module Main exposing (..)
 
 import AstCodec
+import AstCodecV1
 import Benchmark exposing (Benchmark, benchmark, describe)
 import Benchmark.Runner exposing (BenchmarkProgram)
 import Elm.Parser
 import Elm.Processing
 import Serialize as S
+import SerializeV1
 
 
 suite : Benchmark
 suite =
-    describe "Array"
+    describe "Benchmarks"
         [ describe "Encoding"
             [ benchmark "bytes" <|
                 \_ -> code |> Result.map (S.encodeToBytes AstCodec.file)
+            , benchmark "bytes v1" <|
+                \_ -> code |> Result.map (SerializeV1.encodeToBytes AstCodecV1.file)
             , benchmark "json" <|
                 \_ -> code |> Result.map (S.encodeToJson AstCodec.file)
             , benchmark "string" <|
                 \_ -> code |> Result.map (S.encodeToString AstCodec.file)
+            , benchmark "string v1" <|
+                \_ -> code |> Result.map (SerializeV1.encodeToString AstCodecV1.file)
             ]
+        , decodingBenchmarks
         ]
+
+
+decodingBenchmarks =
+    code
+        |> Result.map
+            (\file ->
+                let
+                    bytesData =
+                        S.encodeToBytes AstCodec.file file
+
+                    jsonData =
+                        S.encodeToJson AstCodec.file file
+
+                    stringData =
+                        S.encodeToString AstCodec.file file
+                in
+                [ benchmark "bytes" <|
+                    \_ -> S.decodeFromBytes AstCodec.file bytesData
+                , benchmark "bytes v1" <|
+                    \_ -> SerializeV1.decodeFromBytes AstCodecV1.file bytesData
+                , benchmark "json" <|
+                    \_ -> S.decodeFromJson AstCodec.file jsonData
+                , benchmark "string" <|
+                    \_ -> S.decodeFromString AstCodec.file stringData
+                , benchmark "string v1" <|
+                    \_ -> SerializeV1.decodeFromString AstCodecV1.file stringData
+                ]
+            )
+        |> Result.toMaybe
+        |> Maybe.withDefault []
+        |> describe "Decoding"
 
 
 main : BenchmarkProgram
